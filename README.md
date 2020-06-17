d0e7ef696cf6b8803923858e751b2a48615f7b6b
Andrew Block <andy.block@gmail.com>
2020-06-17 13:28:12 -0400

    Working implementation

--- README.md
+++ README.md
@@ -1,4 +1,213 @@
 openshift-aws-iam-webhook-integration
 ===================================
 
-Integration with AWS IAM Roles for accessing resources using the AWS Security Token Service (STS) in OpenShift
+Integration with AWS IAM Roles for accessing resources using the AWS Security Token Service (STS) in OpenShift using [bound service accounts](https://docs.openshift.com/container-platform/4.4/authentication/bound-service-account-tokens.html)
+
+## Overview
+
+The contents within the repository prepare an Amazon Web Services (AWS) and OpenShift Container Platform (OCP) environment to participate in [Fine grained IAM Roles for Service Accounts](https://aws.amazon.com/blogs/opensource/introducing-fine-grained-iam-roles-service-accounts/). This enables access to AWS services through the use of a service account bound to an AWS IAM role instead of using hard coded keys.
+
+This integration is enabled to the use of a [MutatingWebhookConfiguration](https://kubernetes.io/docs/reference/access-authn-authz/extensible-admission-controllers/) and [amazon-eks-pod-identity-webhook](https://github.com/aws/amazon-eks-pod-identity-webhook/blob/master/SELF_HOSTED_SETUP.md) project. 
+
+As part of the deployment of the webhook, any `ServiceAccount` in any project that is annotated with a a specific key (`sts.amazonaws.com` for this specific implementation) containing an IAM role will have a STS token automatically injected into all pods that make use of the Service Account. 
+
+### Demonstration of Functionality
+
+To demonstrate this type of integration, an simple application that contains the `awscli` tool will be deployed to showcase how it can access the contents of a specific AWS bucket with read and write access. These assets will be deployed in a separate project called `sample-iam-webhook-app`. A service account will be annotated with the name of the role that has a fine grained set of permissions to access this bucket.
+
+## Components
+
+### AWS
+
+To enable integration between OpenShift and AWS, an OIDC Identity Provider is used in order to enable service accounts to assume IAM roles. A portion of the configuration to the OIDC Identity Provider is stored in separate publicly accessible S3 bucket. 
+
+Any IAM role that is used by applications in OpenShift using this functionality need to include a 
+
+In total, the following AWS resources are created to not only support the baseline functionality to enable supporting assuming IAM Roles for applications, but also the demonstration application itself:
+
+* 2 S3 Buckets
+    * Assets to support the OIDC Identity Provider
+    * Demonstration of accessing an S3 bucket by an application within OpenShift
+* 1 OIDC Identity Provider
+* 1 Policy to grant access to the aforementioned application S3 bucket
+* 1 IAM Role
+    * Association to the proceeding Policy
+    * Trust Policy referencing the OIDC identity provider
+
+By default, the Trust Policy that is configured allows access to bind to the IAM role from any Service Account that is created within OpenShift as shown below:
+
+```
+{
+ "Version": "2012-10-17",
+ "Statement": [
+  {
+   "Effect": "Allow",
+   "Principal": {
+    "Federated": "<OIDC_PROVIDER_ARN>"
+   },
+   "Action": "sts:AssumeRoleWithWebIdentity"
+  }
+ ]
+}
+```
+
+Additional configurations can be applied to the trust policy to restrict to either a specific namespace or namespaces along with a Service Account within a a namespace. 
+
+### OpenShift
+
+The primary integration between OpenShift and AWS is through a flag set in the in the [kube-apiserver](https://kubernetes.io/docs/reference/command-line-tools-reference/kube-apiserver/) with a reference to the OIDC Identity Provider endpoint created previously. Within OpenShift, this is configured on the `Authentication` resource field `serviceAccountIssuer` as shown below:
+
+```
+apiVersion: config.openshift.io/v1
+kind: Authentication
+metadata:
+  annotations:
+    release.openshift.io/create-only: "true"
+  creationTimestamp: "2020-06-18T13:39:28Z"
+  generation: 1
+  name: cluster
+  resourceVersion: "13601"
+  selfLink: /apis/config.openshift.io/v1/authentications/cluster
+  uid: 8fefaa1e-1778-4666-bf5d-76520a020a75
+spec:
+  serviceAccountIssuer: https://s3.us-east-1.amazonaws.com/my-oidc-bucket
+status:
+  integratedOAuthMetadata:
+    name: oauth-openshift
+```
+
+With OpenShift integrated to the OIDC identity provider, the a [MutatingWebhookConfiguration](https://kubernetes.io/docs/reference/access-authn-authz/extensible-admission-controllers/) along with the [amazon-eks-pod-identity-webhook](https://github.com/aws/amazon-eks-pod-identity-webhook/blob/master/SELF_HOSTED_SETUP.md) must be deployed. It is deployed in a project called `pod-identity-webhook`.
+
+Finally, an application to demonstrate this functionality is deployed in a project called `sample-iam-webhook-app`. A service account is annotated with the ARN of the IAM role that has been configured with the appropriate access policies as shown below:
+
+```
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  annotations:
+    sts.amazonaws.com/role-arn: "<ROLE_ARN>"
+  name: s3-manager
+  namespace: sample-iam-webhook-app
+```
+
+With all of those components in place, an application can that is able to consume an AWS token is deployed using the previously created service account.
+
+## Running the Project
+
+### Prerequisites
+
+The following are a set of prerequisites prior to deploying the sample project
+
+1. Access to AWS to manage IAM permissions and S3 buckets
+2. AWS CLI installed and configured
+3. OpenShift Command Line Tool installed
+4. `cluster-admin` privileged on the OpenShift Environment
+3. JQ
+
+A script called [iam-webhook-s3-example.sh](iam-webhook-s3-example.sh) is used to deploy the solution. The following arguments are required:
+
+1. `--s3-bucket-name` - Name of the S3 bucket to be created and accessed by the application
+2. `--oidc-s3-bucket-name` - Name of the OIDC S3 bucket to be created to store OIDC configuration
+3. `--aws-region` - Name of the AWS region to target
+
+### Script Execution
+
+Execute the script
+
+```
+$ ./iam-webhook-s3-example.sh --s3-bucket-name <bucket_name> --oidc-s3-bucket-name <oidc_bucket_name> --aws-region <aws_region>
+```
+
+### Verification
+
+Once complete, the two projects will have been created along with the various assets that contain the webhook pod in the `pod-identity-webhook` project and the `sample-iam-webhook-app` containing the sample application.
+
+Change in to the `sample-iam-webhook-app` project:
+
+```
+$ oc project sample-iam-webhook-app
+```
+
+Verify a pod is running and confirm that the AWS token has been mounted into the pod:
+
+```
+$ oc get pod -l app.kubernetes.io/component=app -o yaml
+```
+
+Of importance is the Service Account Token that is automatically injected into the pod by the webhook:
+
+```
+volumes:
+- name: aws-iam-token
+  projected:
+    defaultMode: 420
+    sources:
+    - serviceAccountToken:
+        audience: sts.amazonaws.com
+        expirationSeconds: 86400
+        path: token
+  ```
+
+  Along with the associated `volumeMount`
+
+  ```
+- mountPath: /var/run/secrets/eks.amazonaws.com/serviceaccount
+  name: aws-iam-token
+  readOnly: true
+  ```
+
+The following environment variable are also injected:
+
+```
+env:
+  - name: AWS_ROLE_ARN
+    value: <arn>
+  - name: AWS_WEB_IDENTITY_TOKEN_FILE
+    value: /var/run/secrets/eks.amazonaws.com/serviceaccount/token
+```
+
+The AWS CLI that is part of this sample application leverages the `AWS_WEB_IDENTITY_TOKEN_FILE` environment variable during execution for requests requiring authentication.
+
+Confirm the ability to validate the integration of the solution by removing into the running pod:
+
+```
+$ oc rsh $(oc get pod -l app.kubernetes.io/component=app -o jsonpath='{ .items[*].metadata.name }')
+```
+
+Verify the ability to list S3 buckets:
+
+```
+$ aws s3 ls
+```
+
+Verify that a file can be written to the bucket.
+
+echo "Testing" > /tmp/test.txt
+
+Upload the file to the bucket
+
+```
+$ aws s3 cp /tmp/test.txt s3://<bucketname>/test.txt
+```
+
+## Troubleshooting
+
+There may be circumstances for which the integration will not succeed as expected. The following are common solutions to resolve these issues:
+
+### Various exceptions when executing AWS CLI commands in the pod
+
+There is the potential for a race condition to occur when the appropriate Service Account token is not injected into the application pod properly. This will resolve the pod mutation from not occurring or the proper token from being injected. To resolve this issue, perform the following tasks.
+
+1. Delete the webhook
+
+```
+$ oc delete pod -n pod-identity-webhook  -l=app.kubernetes.io/component=webhook --all
+```
+
+2. Delete the application pod
+
+```
+$ oc delete pod -n sample-iam-webhook-app  -l=app.kubernetes.io/component=app --all
+```
+
+3. Retry the desired action
--- iam-webhook-s3-example.sh
+++ iam-webhook-s3-example.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
- 
-HOME_DIR=`eval 'echo $HOME'`
+#set -x
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 AWS_CREDENTIAL_FILE=$HOME_DIR/.aws/credentials
 AWS_CONFIG_FILE=$HOME_DIR/.aws/config
-AWS_PROFILE=""
+#AWS_PROFILE=""
+AWS_REGION=us-east-1
 OC_CONFIG_FILE=$HOME_DIR/.kube/config
 WEBHOOK_DIR=$HOME_DIR/amazon-eks-pod-identity-webhook
+ASSETS_DIR=${DIR}/runtime-assets
+CLIENT_ID=sts.amazonaws.com
+BIN_DIR=${DIR}/bin
+POD_IDENTITY_WEBHOOK_NAMESPACE="pod-identity-webhook"
+OS=$(echo $(uname) | awk '{print tolower($0)}')
 
 while [[ $# -gt 0 ]]; do
   ARG="$1"
@@ -45,6 +53,11 @@ while [[ $# -gt 0 ]]; do
       shift
       shift
       ;;
+    --oidc-s3-bucket-name)
+      OIDC_S3_BUCKET_NAME="$2"
+      shift
+      shift
+      ;;
     *)
       POSITIONAL+=("$1")
       shift
@@ -59,50 +72,84 @@ command -v aws >/dev/null 2>&1 || { echo >&2 "AWS CLI is required but not instal
 # Check if oc cli tool is installed
 command -v aws >/dev/null 2>&1 || { echo >&2 "AWS CLI is required but not installed.  Aborting."; exit 1; } 
 
+# Check if jq tool is installed
+command -v jq >/dev/null 2>&1 || { echo >&2 "jq is required but not installed.  Aborting."; exit 1; } 
+
+
 # Checking AWS Credentials
-if [ ! -e $AWS_CREDENTIAL_FILE ] && [ -z `echo $AWS_ACCESS_KEY_ID` ] && [ -z `echo $AWS_SECRET_ACCESS_KEY`]; then
-  echo "AWS CLI requires a credentials file in ~/.aws/credentials or set as environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY. Ensure credentials are set appropriately and try again."
-  exit 1
-fi
+#if [ ! -e $AWS_CREDENTIAL_FILE ] && [ -z `echo $AWS_ACCESS_KEY_ID` ] && [ -z `echo $AWS_SECRET_ACCESS_KEY`]; then
+#  echo "AWS CLI requires a credentials file in ~/.aws/credentials or set as environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY. Ensure credentials are set appropriately and try again."
+#  exit 1
+#fi
 
 # Checking AWS configs
-if [ ! -e $AWS_CONFIG_FILE ] || [ -z $AWS_CONFIG_FILE ]; then
-  if [ -z $AWS_REGION ]; then
-    echo "No AWS config was found and AWS region was not specified. Please configure aws cli with default region or specify --region at run time."
-    exit 1
-  fi
-  if [ -z $AWS_OUTPUT_FORMAT ]; then
-    echo "No AWS config was found nor was output format specified at run time. Defaulting to json."
-    AWS_OUTPUT_FORMAT='json'
-  fi
-fi
+#if [ ! -e $AWS_CONFIG_FILE ] || [ -z $AWS_CONFIG_FILE ]; then
+#  if [ -z $AWS_REGION ]; then
+#    echo "No AWS config was found and AWS region was not specified. Please configure aws cli with default region or specify --region at run time."
+#    exit 1
+#  fi
+#  if [ -z $AWS_OUTPUT_FORMAT ]; then
+#    echo "No AWS config was found nor was output format specified at run time. Defaulting to json."
+#    AWS_OUTPUT_FORMAT='json'
+#  fi
+#fi
 
 # Checking OC Configs
-if [ -z `echo $KUBECONFIG` ] && [ -z $OC_CONFIG_FILE ]; then
-  echo "kubeconfig not found. Set kubeconfig environment variable or pass config to command line with --oc-config-file."
-  exit 1
-fi
+#if [ -z `echo $KUBECONFIG` ] && [ -z $OC_CONFIG_FILE ]; then
+#  echo "kubeconfig not found. Set kubeconfig environment variable or pass config to command line with --oc-config-file."
+#  exit 1
+#fi
+
+
 
 #Check if bucket name was specified
-if [ -z $S3_BUCKET_NAME ]; then
-  echo "--s3-bucket-name is required. Aborting."
+if [ -z $S3_BUCKET_NAME ] || [ -z $OIDC_S3_BUCKET_NAME ]; then
+  echo "--s3-bucket-name and --oidc-s3-bucket-name is required. Aborting."
   exit 1
 fi
 
-#Create S3 Bucket
-aws s3api create-bucket --bucket $S3_BUCKET --region $AWS_REGION
-export S3_BUCKET=$S3_BUCKET
-export HOSTNAME=s3.$AWS_REGION.amazonaws.com
-export ISSUER_HOSTPATH=$HOSTNAME/$S3_BUCKET
+LOCATIONCONSTRAINT_OPTION=""
+HOSTNAME=s3-$AWS_REGION.amazonaws.com
+BUCKET_POLICY_NAME=${S3_BUCKET_NAME}-policy
+BUCKET_ROLE_NAME=${S3_BUCKET_NAME}-role
+
+# US EAST 1 Modifications
+if [ "${AWS_REGION}" != "us-east-1" ]; then
+  LOCATIONCONSTRAINT_OPTION="LocationConstraint=${AWS_REGION}"
+  HOSTNAME=s3.$AWS_REGION.amazonaws.com
+fi
+
+ISSUER_HOSTPATH=$HOSTNAME/$OIDC_S3_BUCKET_NAME
+
+existing_oidc_s3_bucket=$(aws s3api list-buckets --query "Buckets[?Name=='${OIDC_S3_BUCKET_NAME}'].Name | [0]" --out text)
+if [ $existing_oidc_s3_bucket == "None" ]; then
+echo "Creating OIDC S3 Bucket: '${OIDC_S3_BUCKET_NAME}"
+aws s3api create-bucket --bucket $OIDC_S3_BUCKET_NAME --create-bucket-configuration "${LOCATIONCONSTRAINT_OPTION}" > /dev/null
+fi
+
+existing_s3_bucket=$(aws s3api list-buckets --query "Buckets[?Name=='${S3_BUCKET_NAME}'].Name | [0]" --out text)
+if [ $existing_s3_bucket == "None" ]; then
+echo "Creating Application S3 Bucket: '${S3_BUCKET_NAME}'"
+aws s3api create-bucket --bucket $S3_BUCKET_NAME --create-bucket-configuration "${LOCATIONCONSTRAINT_OPTION}" > /dev/null
+fi
+
+
+# Create Runtime assets directory if it does not exist
+if [ ! -d "${ASSETS_DIR}" ]; then
+  mkdir -p ${ASSETS_DIR}
+fi
 
 #Get OpenShift keys
-PRIV_KEY="sa-signer.key"
-PUB_KEY="sa-signer.key.pub"
 PKCS_KEY="sa-signer-pkcs8.pub"
-oc get -n openshift-kube-apiserver cm -o json bound-sa-token-signing-certs | jq -r '.data["service-account-001.pub"]' > $PKCS_KEY
+oc get -n openshift-kube-apiserver cm -o json bound-sa-token-signing-certs | jq -r '.data["service-account-001.pub"]' > "${ASSETS_DIR}/${PKCS_KEY}"
+
+if [ $? -ne 0 ]; then
+  echo "Error retrieving Kube API Signer CA"
+  exit 1
+fi
 
 # Create OIDC documents
-cat <<EOF > discovery.json
+cat <<EOF > ${ASSETS_DIR}/discovery.json
 {
     "issuer": "https://$ISSUER_HOSTPATH/",
     "jwks_uri": "https://$ISSUER_HOSTPATH/keys.json",
@@ -123,21 +170,26 @@ cat <<EOF > discovery.json
 }
 EOF
 
-go run $WEBHOOK_DIR/hack/self-hosted/main.go -key $PKCS_KEY  | jq '.keys += [.keys[0]] | .keys[1].kid = ""' > keys.json
+if [ ! -f "${BIN_DIR}/self-hosted-${OS}" ]; then
+  echo "Could not locate self hosted binary"
+  exit 1
+fi
+
+"${BIN_DIR}/self-hosted-${OS}" -key "${ASSETS_DIR}/${PKCS_KEY}"  | jq '.keys += [.keys[0]] | .keys[1].kid = ""' > "${ASSETS_DIR}/keys.json"
 
 # Copy files to S3 Bucket
-aws s3 cp --acl public-read ./discovery.json s3://$S3_BUCKET/.well-known/openid-configuration
-aws s3 cp --acl public-read ./keys.json s3://$S3_BUCKET/keys.json
-
+echo "Uploading configurations to OIDC S3 Bucket"
+aws s3 cp --acl public-read "${ASSETS_DIR}/discovery.json" s3://$OIDC_S3_BUCKET_NAME/.well-known/openid-configuration > /dev/null
+aws s3 cp --acl public-read "${ASSETS_DIR}/keys.json" s3://$OIDC_S3_BUCKET_NAME/keys.json > /dev/null
 
 # Create OIDC Provider
-FINGERPRINT=`echo | openssl s_client -servername s3.us-east-1.amazonaws.com -showcerts -connect s3.us-east-1.amazonaws.com:443 2>/dev/null | openssl x509 -fingerprint -noout | sed s/://g | sed 's/.*=//'`
+FINGERPRINT=`echo | openssl s_client -servername ${HOSTNAME} -showcerts -connect ${HOSTNAME}:443 2>/dev/null | openssl x509 -fingerprint -noout | sed s/://g | sed 's/.*=//'`
 
-cat <<EOF > create-open-id-connect-provider.json
+cat <<EOF > ${ASSETS_DIR}/create-open-id-connect-provider.json
 {
     "Url": "https://$ISSUER_HOSTPATH",
     "ClientIDList": [
-        ""
+        "$CLIENT_ID"
     ],
     "ThumbprintList": [
         "$FINGERPRINT"
@@ -145,4 +197,163 @@ cat <<EOF > create-open-id-connect-provider.json
 }
 EOF
 
-aws iam create-open-id-connect-provider --cli-input-json file://$HOME_DIR/create-open-id-connect-provider.json
+OIDC_IDENTITY_PROVIDER_ARN=$(aws iam list-open-id-connect-providers --query "OpenIDConnectProviderList[?ends_with(Arn, '/${OIDC_S3_BUCKET_NAME}')]".Arn --out text)
+
+if [ "${OIDC_IDENTITY_PROVIDER_ARN}" != "" ]; then
+  echo "Deleting existing open id connect identity provider"
+  aws iam delete-open-id-connect-provider --open-id-connect-provider-arn=${OIDC_IDENTITY_PROVIDER_ARN}
+fi
+
+echo "Creating Identity Provider"
+OIDC_IDENTITY_PROVIDER_ARN=$(aws iam create-open-id-connect-provider --cli-input-json file://${ASSETS_DIR}/create-open-id-connect-provider.json)
+
+
+cat <<EOF > ${ASSETS_DIR}/trust-policy.json
+{
+ "Version": "2012-10-17",
+ "Statement": [
+  {
+   "Effect": "Allow",
+   "Principal": {
+    "Federated": "$OIDC_IDENTITY_PROVIDER_ARN"
+   },
+   "Action": "sts:AssumeRoleWithWebIdentity"
+  }
+ ]
+}
+EOF
+
+cat <<EOF > ${ASSETS_DIR}/bucket-policy.json
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:ListAllMyBuckets"
+      ],
+      "Resource": "arn:aws:s3:::*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": ["s3:ListBucket"],
+      "Resource": ["arn:aws:s3:::${S3_BUCKET_NAME}"]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:DeleteObject"
+      ],
+      "Resource": ["arn:aws:s3:::${S3_BUCKET_NAME}/*"]
+    }
+  ]
+}
+EOF
+
+policy_arn=$(aws iam list-policies --query "Policies[?PolicyName=='${BUCKET_POLICY_NAME}'].{ARN:Arn}" --output text)
+
+if [ "${policy_arn}" != "" ]; then
+   # Check to see how many policies we have
+  policy_versions=$(aws iam list-policy-versions --policy-arn=${policy_arn} --query "Versions[] | length(@)")
+
+  if [ $policy_versions -gt 1 ]; then
+    oldest_policy_version=$(aws iam list-policy-versions --policy-arn=${policy_arn} --query "Versions[-1].VersionId")
+
+    echo "Deleting Oldest Policy Version: ${oldest_policy_version}"
+    aws iam delete-policy-version --policy-arn=${policy_arn} --version-id=${oldest_policy_version}
+  fi
+
+  echo "Creating new Policy Version"
+  aws iam create-policy-version --policy-arn ${policy_arn} --policy-document file://${ASSETS_DIR}/bucket-policy.json --set-as-default > /dev/null
+
+else
+  echo "Creating new IAM Policy: '${BUCKET_POLICY_NAME}"
+  policy_arn=$(aws iam create-policy --policy-name ${BUCKET_POLICY_NAME} --policy-document file://${ASSETS_DIR}/bucket-policy.json --query Policy.Arn --output text)
+fi
+
+role_arn=$(aws iam list-roles --query "Roles[?RoleName=='${BUCKET_ROLE_NAME}'].{ARN:Arn}" --out text)
+
+if [ "${role_arn}" == "" ]; then
+  echo "Creating Assume Role Policy"
+  role_arn=$(aws iam create-role --role-name ${BUCKET_ROLE_NAME} --assume-role-policy-document file://${ASSETS_DIR}/trust-policy.json --query Role.Arn --output text)
+else
+  echo "Updating Assime Role Policy"
+  aws iam update-assume-role-policy --role-name ${BUCKET_ROLE_NAME} --policy-document file://${ASSETS_DIR}/trust-policy.json > /dev/null
+fi
+
+echo "Attaching Policy to IAM Role"
+aws iam attach-role-policy --role-name ${BUCKET_ROLE_NAME} --policy-arn ${policy_arn} > /dev/null
+
+echo "Creating OpenShift Manifests"
+until oc apply -f "${DIR}/manifests/pod-identity-webhook" 2>/dev/null; do sleep 2; done
+
+echo "Waiting for webhook pod to be deployed"
+oc rollout status deploy/pod-identity-webhook -n $POD_IDENTITY_WEBHOOK_NAMESPACE
+
+echo "Waiting for CSR's to be Created"
+until [ "$(oc get csr -o jsonpath="{ .items[?(@.spec.username==\"system:serviceaccount:$POD_IDENTITY_WEBHOOK_NAMESPACE:pod-identity-webhook\")].metadata.name}")" != "" ]; do sleep 2; done
+
+echo "Approving CSR's"
+for csr in `oc get csr -n ${POD_IDENTITY_WEBHOOK_NAMESPACE} -o name`; do
+  oc adm certificate approve $csr
+done
+
+
+echo "Patching OpenShift Cluster Authentication"
+oc patch authentication.config.openshift.io cluster --type "json" -p="[{\"op\": \"replace\", \"path\": \"/spec/serviceAccountIssuer\", \"value\":\"https://${ISSUER_HOSTPATH}\"}]"
+
+echo "Creating Mutating Webhook"
+CA_BUNDLE=$(oc get configmap -n kube-system extension-apiserver-authentication -o=jsonpath='{.data.client-ca-file}' | base64 | tr -d '\n')
+
+(
+cat <<EOF
+apiVersion: admissionregistration.k8s.io/v1beta1
+kind: MutatingWebhookConfiguration
+metadata:
+  name: pod-identity-webhook
+  namespace: pod-identity-webhook
+webhooks:
+- name: pod-identity-webhook.amazonaws.com
+  failurePolicy: Ignore
+  clientConfig:
+    service:
+      name: pod-identity-webhook
+      namespace: pod-identity-webhook
+      path: "/mutate"
+    caBundle: ${CA_BUNDLE}
+  rules:
+  - operations: [ "CREATE" ]
+    apiGroups: [""]
+    apiVersions: ["v1"]
+    resources: ["pods"]
+EOF
+ ) | oc apply -f-
+
+echo "Rolling Webhook pods"
+oc delete pods -n ${POD_IDENTITY_WEBHOOK_NAMESPACE} -l=app.kubernetes.io/component=webhook
+
+echo "Waiting a moment...."
+sleep 10
+
+echo "Creating Sample Application Resources"
+oc apply -f "${DIR}/manifests/sample-app/namespace.yaml"
+
+(
+cat <<EOF
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  annotations:
+    sts.amazonaws.com/role-arn: "${role_arn}"
+  name: s3-manager
+  namespace: sample-iam-webhook-app
+EOF
+ ) | oc apply -f-
+
+oc apply -f "${DIR}/manifests/sample-app/deployment.yaml"
+
+
+echo
+echo "Setup Completed Successfully!"
